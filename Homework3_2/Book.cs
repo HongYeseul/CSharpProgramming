c0cd0998684922e3f9cbb622595503bd0ae0eb8c
HongYeseul <ghd8119@naver.com>
2020-10-08 04:16:03 +0900

    HomeWork 3-2 v0.1

---
+++ Homework3_2/Book.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework3_2
+{
+    class Book
+    {
+        private string name;
+        private int number;
+
+        public Book() { }
+
+        public Book(string str, int no)
+        {
+            this.name = str;
+            this.number = no;
+        }
+
+        public string getName
+        {
+            get
+            {
+                return name;
+            }
+        }
+        public int getID
+        {
+            get
+            {
+                return number;
+            }
+        }
+    }
+}
