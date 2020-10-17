a4a7ec28005d00811c582f1a744a42626e0d554a
ghd8119 <ghd8119@naver.com>
2020-10-17 16:04:33 +0900

    Start Homework4 - PC program

---
+++ Homework4/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Homework4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //PC방 시스템 만들기
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
