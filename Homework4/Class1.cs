6c0b6ad8642dffbe29debe2085ece689ce7d83a8
HongYeseul <ghd8119@naver.com>
2020-10-18 19:27:56 +0900

    Homework4 - delegate, event 해야함

---
+++ Homework4/Class1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Text;
+
+namespace Homework4
+{
+    delegate void PCINFO();
+    delegate void SETPCINFO(int id, String cpu, int mem);
+    delegate void StartTime(long time);
+    delegate int StopTime(long time);
+    delegate int GetTotal();
+
+    class PCInfo
+    {
+        int iId;
+        public String strCpu;
+        int iMem;
+        long IStart;
+        long IStop;
+        public int iTotal;
+        public event EventHandler TOTAL;
+
+        public PCInfo()
+        {
+            //멤버 변수 값 초기화
+        }
+
+        public void setPCInfo(int id, String cpu, int mem)
+        {
+            this.iId = id;
+            this.strCpu = cpu;
+            this.iMem = mem;
+        }
+
+        public void start(long time)
+        {
+            // 사용 시작 시간 설정
+            this.IStart = time;
+        }
+        public int stop(long time)
+        {
+            int cost;
+            this.IStop = time;
+            // 사용 종료시간 받아서 구하고 요금 계산해서 반환
+            cost = (int)((double)(IStop - IStart) / 10000000.0F)*500;
+            return cost;
+        }
+        public int getTotal()
+        {
+            // 프로그램 시작 이후에 PC 객체를 사용한 금액의 합을 반환
+            long time = System.DateTime.Now.Ticks;
+            iTotal = (int)((double)(time - IStart) / 10000000.0F) * 500;
+            return iTotal;
+        }
+    }
+}
--- Homework4/Program.cs
+++ Homework4/Program.cs
@@ -1,13 +1,84 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
 
 namespace Homework4
 {
-    class Program
+    class Manager
     {
+
         static void Main(string[] args)
-        {
+        { 
+
+
             //PC방 시스템 만들기
-            Console.WriteLine("Hello World!");
+            /*
+            long start = System.DateTime.Now.Ticks;
+            Thread.Sleep(3000);
+            long stop = System.DateTime.Now.Ticks;
+            Console.WriteLine((double)(stop - start) / 10000000.0F + "초");
+
+            */
+
+            string temp;
+            Console.Write("등록할 컴퓨터의 수량을 입력하세요: ");
+            temp = Console.ReadLine();
+            int count = Convert.ToInt32(temp);
+            List<PCInfo> list = new List<PCInfo>();
+
+            for (int i=0; i<count; i++)
+            {
+                PCInfo t = new PCInfo();
+                Console.WriteLine("PC " + i + "번");
+                Console.Write("컴퓨터의 기종(CPU)을 입력하세요 : ");
+                temp = Console.ReadLine();
+                string cpu = Convert.ToString(temp);
+                Console.Write("메모리의 양을 입력하세요(GB) : ");
+                temp = Console.ReadLine();
+                int mem = Convert.ToInt32(temp);
+
+                //delegate로 접근할 것
+                t.setPCInfo(i, cpu, mem);
+                list.Add(t);
+            }
+
+            while (true)
+            {
+                Console.WriteLine("=================");
+                Console.WriteLine("1. 컴퓨터 사용 시작 [0-2]");
+                Console.WriteLine("2. 컴퓨터 사용 종료 [0-2]");
+                Console.WriteLine("3. 매출정보 출력");
+                Console.WriteLine("4. 프로그램 종료");
+                Console.WriteLine("=================");
+                Console.Write("메뉴의 번호를 입력하세요(1-4) : ");
+                temp = Console.ReadLine();
+                int n = Convert.ToInt32(temp);
+                int cN;
+
+                switch (n)
+                {
+                    case 1: // 사용 시작
+                        Console.Write("컴퓨터의 번호를 입력하세요 : ");
+                        temp = Console.ReadLine();
+                        cN = Convert.ToInt32(temp);
+                        list[cN].start(System.DateTime.Now.Ticks);
+                        break;
+                    case 2: // 사용 종료
+                        Console.Write("컴퓨터의 번호를 입력하세요 : ");
+                        temp = Console.ReadLine();
+                        cN = Convert.ToInt32(temp);
+                        int cost = list[cN].stop(System.DateTime.Now.Ticks);
+                        Console.WriteLine("사용금액은 " + cost + "원 입니다.");
+                        break;
+                    case 3: // 정보 출력
+                        
+                        break;
+                    case 4: // 프로그램 종료
+                        return;
+                }
+            }
         }
     }
 }
