1239341edcece9f8b5dff1d06e4373723283edc8
ghd8119 <ghd8119@naver.com>
2020-10-27 16:55:44 +0900

    Homework5 : case 7 -ing

--- Homework5/Program.cs
+++ Homework5/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 
 namespace Homework5
 {
@@ -62,7 +63,7 @@ namespace Homework5
                                         i++;
                                     }
                                 }
-                        }
+                        } i = 0;
                         break;
                     case 2:
                         Console.WriteLine("13~19세 사이인 청소년을 리스트에서 검색 (group-by 사용)");
@@ -78,21 +79,51 @@ namespace Homework5
                             if (person.Key)
                                 foreach (var p in person.human)
                                 {
-                                    if (i < 2)
-                                    {
-                                        Console.WriteLine(p.SSN + "," + p.Name + "," + p.Address + "," + p.Age);
-                                        i++;
-                                    }
+                                    Console.WriteLine(p.SSN + "," + p.Name + "," + p.Address + "," + p.Age);
                                 }
                         }
                         break;
-                    case 4:
+                    case 3: // 모두 나이가 10 세이상인지 체크하고 그 결과 출력
+                        Console.WriteLine("모두 나이가 10 세이상인지 체크하고 그 결과 출력");
+                        var third = from correctPerson in listPersonsInCity
+                                    select correctPerson.Age >= 10;
+                        if(third.ToList().Count == listPersonsInCity.Count)
+                            Console.WriteLine("모두 나이가 10세 이상입니다.");
                         break;
-                    case 5:
+                    case 4: // 모든 사람을 대상으로 평균 연령 구하기 (Average 표준연산자 사용)
+                        Console.WriteLine("모든 사람을 대상으로 평균 연령 구하기(Average 표준연산자 사용)");
+                        var d = from correctPerson in listPersonsInCity
+                                select correctPerson.Age;
+                        double ave = d.Average();
+                        Console.WriteLine("평균 연령은 "+ave+"세 입니다");
                         break;
-                    case 6:
+                    case 5: // SAM 이란 이름을 검색 하고 삭제하기
+                        Console.WriteLine("SAM 이란 이름을 검색 하고 삭제하기");
+                        var e = from correctPerson in listPersonsInCity
+                                where !correctPerson.Name.Contains("SAM")
+                                select correctPerson;
+                        listPersonsInCity = e.ToList();
+                        //for (int j = 0; j < listPersonsInCity.Count; j++)
+                        //Console.WriteLine(listPersonsInCity[j].SSN + "," + listPersonsInCity[j].Name + "," + listPersonsInCity[j].Address + "," + listPersonsInCity[j].Age);
+                        Console.WriteLine("SAM이 들어간 이름의 데이터가 삭제 되었습니다.");
                         break;
-                    case 7:
+                    case 6: // Smith 인덱스 위치 알아내기
+                        Console.WriteLine("Smith 인덱스 위치 알아내기");
+                        var f = from correctPerson in listPersonsInCity
+                                where correctPerson.Name.Contains("Smith")
+                                select correctPerson;
+
+                        var thing = f.ToArray<Person>()[0];
+                        int count = 0;
+                        foreach (Person p in listPersonsInCity)
+                        {
+                            if (p.Equals(thing)) break;
+                            count++;
+                        }
+                        Console.WriteLine("Smith의 인덱스 위치는 "+ count);
+                        break;
+                    case 7: //나이가 가장 많은 사람의 이름과 나이 출력
+                        Console.WriteLine("나이가 가장 많은 사람의 이름과 나이 출력");
                         break;
                     case 8:
                         break;
